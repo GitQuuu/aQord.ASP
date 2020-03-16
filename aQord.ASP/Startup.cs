d8748e6d70ed910c64cd693cc46c04638467ecc3
Qu <Quanvle@live.dk>
2020-03-16 13:23:33 +0100

    Create a new MVC project with Individual authentication

---
+++ aQord.ASP/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(aQord.ASP.Startup))]
+namespace aQord.ASP
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
