8ca424810d0244f80cab2e7f50cb366a030c714d
Qu <Quanvle@live.dk>
2020-03-16 14:43:14 +0100

    rename properties Person to People

--- aQord.ASP/Models/IdentityModels.cs
+++ aQord.ASP/Models/IdentityModels.cs
@@ -20,7 +20,7 @@ namespace aQord.ASP.Models
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
-        public DbSet<Person> Persons  { get; set; }
+        public DbSet<Person> PeopleDbSet  { get; set; }
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
