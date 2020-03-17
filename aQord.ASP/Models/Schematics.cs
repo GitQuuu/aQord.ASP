25b382c5a2e63275bc2b3e925750b6c39f0b0aa0
Qu <Quanvle@live.dk>
2020-03-17 11:12:15 +0100

    add Model Schematics with properties for Excel output

---
+++ aQord.ASP/Models/Schematics.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace aQord.ASP.Models
+{
+    public class Schematics
+    {
+        public int Id { get; set; }
+        public string TypeOfWork { get; set; }
+        public string StaffRepresentative { get; set; }
+        public int Year { get; set; }
+        public string Firm { get; set; }
+        public string WorkplaceAddress { get; set; }
+        public string ProjectNumber { get; set; }
+        public int CraftsmanId { get; set; }
+        public string Name { get; set; }
+        public int WeekNumber { get; set; }
+        public double[] AkkordHours { get; set; }
+        public double[] NormalHours { get; set; }
+        public double ShelterRateAmountOfDays { get; set; }
+        public double MileageAllowanceAmountOfKm { get; set; }
+        
+    }
+}
