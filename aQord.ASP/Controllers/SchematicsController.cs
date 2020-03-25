e938527c3d6a3ad708cb9daf4bbdc3229e569cda
Qu <Quanvle@live.dk>
2020-03-25 12:04:29 +0100

    working on Schematics Controller and view without scaffolding

---
+++ aQord.ASP/Controllers/SchematicsController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using aQord.ASP.Models;
+
+namespace aQord.ASP.Controllers
+{
+    public class SchematicsController : Controller
+    {
+        public ApplicationDbContext _dbContext;
+
+        public SchematicsController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+
+        // GET: Schematics
+        public ActionResult Index()
+        {
+            return View(_dbContext.Schematics);
+        }
+    }
+}
