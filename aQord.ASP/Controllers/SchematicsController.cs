61e607037a5df9f9eb5f38d669ab34e093ba9af6
Qu <Quanvle@live.dk>
2020-04-01 13:06:14 +0200

    working on filling form on dropdown selection
    https://entityframework.net/knowledge-base/26031197/fill-textbox-on-dropdown-selection-in-mvc-entity-framework

--- aQord.ASP/Controllers/SchematicsController.cs
+++ aQord.ASP/Controllers/SchematicsController.cs
@@ -24,6 +24,7 @@ namespace aQord.ASP.Controllers
 
         public ActionResult New()
         {
+            ViewData["Id"] = new SelectList(_dbContext.People,"Id","Id");
             return View("SchematicsForm");
         }
 
