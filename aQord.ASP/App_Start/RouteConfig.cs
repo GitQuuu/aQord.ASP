2ebaee1016fd408d0a2e2b99e02e46c7aaae9b4c
Qu <Quanvle@live.dk>
2020-06-09 21:26:51 +0200

    add more to timeline and enable custom route  and redeply to azure from home

--- aQord.ASP/App_Start/RouteConfig.cs
+++ aQord.ASP/App_Start/RouteConfig.cs
@@ -14,11 +14,11 @@ namespace aQord.ASP
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             //Creating a custom route that force the web app to start with danish culture and still keeps the default route incase this one fails  https://stackoverflow.com/questions/1560796/set-culture-in-an-asp-net-mvc-app?rq=1
-            //routes.MapRoute(
-            //    name: "da-DKCulture",
-            //    url: "{language}-{culture}/{controller}/{action}/{id}",
-            //    defaults: new { language = "da", culture = "DK", controller = "Home", action = "Index", id = UrlParameter.Optional }
-            //);
+            routes.MapRoute(
+                name: "da-DKCulture",
+                url: "{language}-{culture}/{controller}/{action}/{id}",
+                defaults: new { language = "da", culture = "DK", controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
 
             routes.MapRoute(
                 "Default",                                              // Route name
