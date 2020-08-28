88511cbab191b2470922bff128338ff8f124a1c8
Qu <Quan0027@elec.techcollege.dk>
2020-08-28 11:23:22 +0200

    RevertingMigration

---
+++ aQord.ASP/Migrations/202008280922099_RevertDb.cs
@@ -0,0 +1,33 @@
+﻿namespace aQord.ASP.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class RevertDb : DbMigration
+    {
+        public override void Up()
+        {
+            DropForeignKey("dbo.SchemaHours", "SchematicsId", "dbo.Schematics");
+            DropIndex("dbo.SchemaHours", new[] { "SchematicsId" });
+            DropTable("dbo.SchemaHours");
+        }
+        
+        public override void Down()
+        {
+            CreateTable(
+                "dbo.SchemaHours",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        SchemaId = c.Int(nullable: false),
+                        AkkordHours = c.Double(nullable: false),
+                        NormalHours = c.Double(nullable: false),
+                        SchematicsId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateIndex("dbo.SchemaHours", "SchematicsId");
+            AddForeignKey("dbo.SchemaHours", "SchematicsId", "dbo.Schematics", "Id", cascadeDelete: true);
+        }
+    }
+}
