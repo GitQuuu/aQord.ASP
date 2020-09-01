d02cb7eefb2db8ff1ec67d9bc42116088176f6a0
Qu <Quan0027@elec.techcollege.dk>
2020-09-01 13:01:56 +0200

    EF- Enable cascade deletion - https://stackoverflow.com/questions/5471374/how-do-you-ensure-cascade-delete-is-enabled-on-a-table-relationship-in-ef-code-f

--- aQord.ASP/Models/Hours.cs
+++ aQord.ASP/Models/Hours.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -12,6 +13,8 @@ namespace aQord.ASP.Models
         public double NormalHours { get; set; }
         public DayOfWeek Day { get; set; }
         // By adding this properties EF will create a columnn that has a relation to Schematics Table in the database
+        // and this annotation to enable cascade deletion
+        [Required]
         public Schematics Schematics { get; set; }
 
         // Convention 2 - https://www.entityframeworktutorial.net/code-first/configure-one-to-many-relationship-in-code-first.aspx
